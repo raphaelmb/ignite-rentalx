4f0b8ad4e43d21964b83ce05810d0320a1540c53
Raphael Berger <raphaelmb@gmail.com>
2022-04-30 13:53:42 -0300

    LSP

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,10 @@
 import Category from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-export default class CategoriesRepository {
+export default class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
