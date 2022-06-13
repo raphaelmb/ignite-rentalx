cea961bb19c6445922f9c2fb17027b6259abceaa
Raphael Berger <raphaelmb@gmail.com>
2022-06-13 17:45:51 -0300

    Test refactoring

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,44 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import UserTokens from "@modules/accounts/infra/typeorm/entities/UserTokens";
+import { IUsersTokensRepository } from "../IUsersTokensRepostory";
+
+export default class UsersTokensRepositoryInMemory
+  implements IUsersTokensRepository
+{
+  usersTokens: UserTokens[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+    Object.assign(userToken, {
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+    this.usersTokens.push(userToken);
+    return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    return this.usersTokens.find(
+      (ut) => ut.user_id === user_id && ut.refresh_token === refresh_token
+    );
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const userToken = this.usersTokens.find((userToken) => userToken.id === id);
+    this.usersTokens.splice(this.usersTokens.indexOf(userToken));
+  }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    return this.usersTokens.find(
+      (userToken) => userToken.refresh_token === refresh_token
+    );
+  }
+}
--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -8,21 +8,25 @@ import fs from "node:fs";
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
   constructor() {
-    nodemailer
-      .createTestAccount()
-      .then((account) => {
-        const transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-        });
-        this.client = transporter;
-      })
-      .catch((err) => console.error(err));
+    this.createClient();
+  }
+
+  private async createClient() {
+    try {
+      const account = await nodemailer.createTestAccount();
+
+      this.client = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+    } catch (err) {
+      console.error(`EtherealMailProvider - Error:\n${err}`);
+    }
   }
 
   async sendMail(
@@ -31,6 +35,9 @@ export default class EtherealMailProvider implements IMailProvider {
     variables: any,
     path: string
   ): Promise<void> {
+    if (!this.client) {
+      await this.createClient();
+    }
     const templateFileContent = fs.readFileSync(path).toString("utf-8");
     const templateParse = handlebars.compile(templateFileContent);
     const templateHTML = templateParse(variables);
